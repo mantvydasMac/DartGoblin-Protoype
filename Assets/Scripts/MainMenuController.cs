b6ace30d548d375ccac02eb0a39dba20223f4383
Vilius Paskonis <vilius.paskonis@gmail.com>
2025-11-26 19:50:15 +0200

    main menu credits

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -11,12 +12,27 @@ public class MainMenuController : MonoBehaviour
     public Button creditsButton;
     public Button quitButton;
 
+    public VisualElement stagesModal;
+    public Button stage1Button;
+    public Button stage2Button;
+
+    public VisualElement creditsModal;
+
+    public List<VisualElement> modals = new List<VisualElement>();
+
     private void Awake()
     {
         ui = GetComponent<UIDocument>().rootVisualElement;
     }
 
     private void OnEnable()
+    {
+        InitMainButtons();
+        InitStagesModal();
+        InitCreditsModal();
+    }
+
+    private void InitMainButtons()
     {
         playButton = ui.Q<Button>("play-button");
         playButton.clicked += OnPlayButtonClicked;
@@ -31,23 +47,71 @@ public class MainMenuController : MonoBehaviour
         quitButton.clicked += OnQuitButtonClicked;
     }
 
+    private void InitStagesModal()
+    {
+        stagesModal = ui.Q<VisualElement>("stages-modal");
+        modals.Add(stagesModal);
+
+        stage1Button = ui.Q<Button>("stage1-button");
+        stage1Button.clicked += OnStage1Clicked;
+
+        stage2Button = ui.Q<Button>("stage2-button");
+        stage2Button.clicked += OnStage2Clicked;
+    }
+
+    private void InitCreditsModal()
+    {
+        creditsModal = ui.Q<VisualElement>("credits-modal");
+        modals.Add(creditsModal);
+    }
+
     private void OnPlayButtonClicked()
     {
-        SceneManager.LoadScene(1);
+        ToggleModal(stagesModal);
     }
 
     private void OnOptionsButtonClicked()
     {
-        Debug.Log("Options button clicked");
+        CloseAllModals();
     }
 
     private void OnCreditsButtonClicked()
     {
-        Debug.Log("Credits button clicked");
+        ToggleModal(creditsModal);
     }
 
     private void OnQuitButtonClicked()
     {
         Application.Quit();
+    }
+
+    private void OnStage1Clicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnStage2Clicked()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    private void CloseAllModals()
+    {
+        foreach (var modal in modals)
+        {
+            modal.RemoveFromClassList("open");
+        }
+    }
+
+    private void ToggleModal(VisualElement modal)
+    {
+        if (modal.ClassListContains("open"))
+        {
+            CloseAllModals();
+            return;
+        }
+
+        CloseAllModals();
+        modal.AddToClassList("open");
     }
 }
