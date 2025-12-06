d6fbcd788de57dfd2eba9ade1cf38215ca02d9ea
mantvydasMac <144117211+mantvydasMac@users.noreply.github.com>
2025-12-06 18:50:24 +0200

    settings

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -11,7 +11,14 @@ public class PauseController : MonoBehaviour
 
     public Button resumeButton;
     public Button mainMenuButton;
+    public Button settingsButton;
     public Button quitButton;
+
+    public VisualElement settingsModal;
+    public Slider masterVolSlider;
+    public Slider sfxVolSlider;
+    public Slider musicVolSlider;
+    public Slider fpsSlider;
 
     private void Awake()
     {
@@ -28,6 +35,11 @@ public class PauseController : MonoBehaviour
 
         mainMenuButton = ui.Q<Button>("main-menu-button");
         mainMenuButton.clicked += onMainMenuButtonClicked;
+
+        settingsButton = ui.Q<Button>("settings-button");
+        settingsButton.clicked += OnSettingsButtonClicked;
+
+        InitSettingsModal();
     }
 
     void Update()
@@ -38,20 +50,112 @@ public class PauseController : MonoBehaviour
             Time.timeScale = paused ? 1 : 0;
 
             if (paused)
+            {
+                if (settingsModal.ClassListContains("open"))
+                {
+                    settingsModal.RemoveFromClassList("open");
+                }
                 pausedPanel.RemoveFromClassList("open");
+            }
             else
+            {
                 pausedPanel.AddToClassList("open");
+            }
         }
+    }
+
+    private void InitSettingsModal()
+    {
+        settingsModal = ui.Q<VisualElement>("settings-modal");
+
+        //volume
+        masterVolSlider = ui.Q<Slider>("master-slider");
+        masterVolSlider.value = PlayerPrefs.GetFloat("masterVolume", 100);
+
+        masterVolSlider.RegisterValueChangedCallback(evt =>
+        {
+            float newValue = evt.newValue;
+
+            AudioManager.Instance.SetMasterVolume(newValue/100);
+
+            PlayerPrefs.SetFloat("masterVolume", newValue);
+            PlayerPrefs.Save();
+        });
+
+        sfxVolSlider = ui.Q<Slider>("sfx-slider");
+        sfxVolSlider.value = PlayerPrefs.GetFloat("sfxVolume", 100);
+
+        sfxVolSlider.RegisterValueChangedCallback(evt =>
+        {
+            float newValue = evt.newValue;
+
+            AudioManager.Instance.SetSFXVolume(newValue/100);
+
+            PlayerPrefs.SetFloat("sfxVolume", newValue);
+            PlayerPrefs.Save();
+        });
+
+        musicVolSlider = ui.Q<Slider>("music-slider");
+        musicVolSlider.value = PlayerPrefs.GetFloat("musicVolume", 100);
+
+        musicVolSlider.RegisterValueChangedCallback(evt =>
+        {
+            float newValue = evt.newValue;
+
+            AudioManager.Instance.SetMusicVolume(newValue/100);
+
+            PlayerPrefs.SetFloat("musicVolume", newValue);
+            PlayerPrefs.Save();
+        });
+
+
+
+        //fps
+        fpsSlider = ui.Q<Slider>("fps-slider");
+        fpsSlider.value = PlayerPrefs.GetInt("fps", 60);
+        Application.targetFrameRate = (int)fpsSlider.value;
+
+        PlayerPrefs.SetInt("fps", (int)fpsSlider.value);
+        PlayerPrefs.Save();
+
+        fpsSlider.RegisterValueChangedCallback(evt =>
+        {
+            float newValue = evt.newValue;
+            Application.targetFrameRate = (int)newValue;
+
+            PlayerPrefs.SetInt("fps", (int)newValue);
+            PlayerPrefs.Save();
+        });
     }
 
     private void OnResumeButtonClicked()
     {
         Time.timeScale = 1;
+        if (settingsModal.ClassListContains("open"))
+        {
+            settingsModal.RemoveFromClassList("open");
+        }
         pausedPanel.RemoveFromClassList("open");
     }
 
     private void onMainMenuButtonClicked()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void OnSettingsButtonClicked()
+    {
+        ToggleModal(settingsModal);
+    }
+
+    private void ToggleModal(VisualElement modal)
+    {
+        if (modal.ClassListContains("open"))
+        {
+            modal.RemoveFromClassList("open");
+            return;
+        }
+
+        modal.AddToClassList("open");
     }
 }
